4f5c320f0e2e03a98b354eeb942c2c9558620fd4
Jack <ahfhafh@gmail.com>
2022-12-05 17:25:08 -0700

    make boards 400px

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className='my-48'>
       <header className='text-center mb-12 font-bold text-2xl'>BattleShlt</header>
-      <div className="App flex flex-row mt-8">
+      <div className="App flex flex-row justify-around mt-8">
         <DndProvider backend={HTML5Backend}>
           <Board1 />
           <Board2 />
